2bfd8e923df075eb1242da69392b862be387cfec
rurru <44536425+rurru@users.noreply.github.com>
2019-04-13 16:06:11 -0700

    Added state to main app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const 
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state= {
+      sessionLength: 25,
+      breakLength: 5,
+      secondsLeft: 25
+    }
+
+  }
   render() {
     return (
       <div className="App">
-        <div className = "display">
+        <div id = "timer" className = "display">
         </div>
-        <div className = "display">
+        <div id = "options" className = "display">
         </div>
       </div>
     );
