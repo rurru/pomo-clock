b05b33551b6a8cb97520f611f4171cd8daaab448
rurru <44536425+rurru@users.noreply.github.com>
2019-04-13 10:26:03 -0700

    Added display class

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <div className = "display">
+          YO
+        </div>
       </div>
     );
   }
